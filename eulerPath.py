140100b185d707f3e7e1731b2d1faa3dac0d9fa3
JoeMansley <87179481+JoeMansley@users.noreply.github.com>
2023-02-11 04:38:34 +0530

    Add files via upload

---
+++ eulerPath.py
@@ -0,0 +1,40 @@
+#Given the adjacency list of a directed graph, output an Euler path. If none exists, output false.
+#Assume vertices have labels 0,1,...n-1
+#Example input would be [{1},{},{0}], which should give output [2,0,1]
+
+from copy import deepcopy
+
+def EulerPath(inputAdjList):
+    adjList=deepcopy(inputAdjList)
+    
+    inDeg=[0]*len(adjList)
+    for a in adjList:
+        for k in a:
+            inDeg[k]+=1
+            
+    v1=0
+    for j,a in enumerate(adjList):
+        d=inDeg[j]-len(a) #in degree - out degree
+        if d==1:
+            v1=j
+            break
+        elif d>1 or d<-1:
+            return False
+
+    def walkFrom(u):
+        walk=[u]
+        while adjList[walk[-1]]:
+            walk.append(adjList[walk[-1]].pop())
+        return walk
+    
+    path=walkFrom(v1)
+    k=0
+    while k<len(path):
+        if adjList[path[k]]:
+            path=path[:k]+walkFrom(path[k])+path[k+1:]
+        else:
+            k+=1
+            
+    if any(adjList):
+        return False
+    return path
